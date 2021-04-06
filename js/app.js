480159e91f5083ba88b8e41815957c761e64a5d0
quadridev <76840924+quadridev@users.noreply.github.com>
2021-04-06 04:18:19 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,41 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-link");
+const links = document.querySelectorAll(".nav-link li");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+});
+
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
+// intro
+
+const slideshowImages = document.querySelectorAll(".intro-slideshow img");
+
+const nextImageDelay = 5000;
+let currentImageCounter = 0; // setting a variable to keep track of the current image (slide)
+
+// slideshowImages[currentImageCounter].style.display = "block";
+slideshowImages[currentImageCounter].style.opacity = 1;
+
+setInterval(nextImage, nextImageDelay);
+
+function nextImage() {
+  // slideshowImages[currentImageCounter].style.display = "none";
+  slideshowImages[currentImageCounter].style.opacity = 0;
+
+  currentImageCounter = (currentImageCounter+1) % slideshowImages.length;
+
+  // slideshowImages[currentImageCounter].style.display = "block";
+  slideshowImages[currentImageCounter].style.opacity = 1;
+}
